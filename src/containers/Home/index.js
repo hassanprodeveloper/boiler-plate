95149e287768d165eeedf78cef11d40687e1360a
hassanakram-n <hassanakram.n@gmail.com>
2020-10-27 10:39:30 +0500

    changes done

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {set_data} from '../../store/action'
+import Header from '../../components/header'
+
 class Home extends React.Component{
     render(){
         console.log('props===>',this.props);
         return(
             <div>
+                <Header />
                 <h2>Home</h2>
                 <button onClick={()=>this.props.set_data()}>Set Data</button>
             </div>
